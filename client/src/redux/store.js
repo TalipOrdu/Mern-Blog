5a8b80dd7360718b9f98b26b69eca4ecd0e0112a
TalipOrdu <ordu.talip007@gmail.com>
2024-08-08 16:53:30 +0300

    Dark Mode

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,28 +1,28 @@
-import { configureStore, combineReducers } from '@reduxjs/toolkit'
-import userReducer from "./user/userSlice";
-import {persistReducer} from 'redux-persist';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import userReducer from './user/userSlice';
+import themeReducer from './theme/themeSlice';
+import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import persistStore from 'redux-persist/es/persistStore';
 
 const rootReducer = combineReducers({
   user: userReducer,
+  theme: themeReducer, // themeReducer burada root seviyesinde olmalı
 });
 
 const persistConfig = {
   key: 'root',
   storage,
-  version:1 
-}
+  version: 1 
+};
 
-const persistedReducer = persistReducer(persistConfig, rootReducer)
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    user: persistedReducer,
-  },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
-      serializableCheck: false, // Disallow RPVT check for simplicity. Alternatively, enable it with caution.
+      serializableCheck: false,
     }),
 });
 
