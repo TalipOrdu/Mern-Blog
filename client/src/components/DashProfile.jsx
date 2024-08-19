11b6c8b910e5de6ee9b02e3912bbaa8bc6f03a9f
TalipOrdu <ordu.talip007@gmail.com>
2024-08-19 16:59:50 +0300

    update user api route

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,28 +1,161 @@
-import { Button, TextInput } from "flowbite-react";
+import { Alert, Button, TextInput } from "flowbite-react";
+import { useEffect, useRef, useState, useCallback } from "react";
 import { useSelector } from "react-redux";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
 export default function DashProfile() {
   const { currentUser } = useSelector((state) => state.user);
+  const [imageFile, setImageFile] = useState(null);
+  const [imageFileUrl, setImageFileUrl] = useState(null);
+  const filePickerRef = useRef();
+  const [imageFileUploadProgress, setImageFileUploadProgress] = useState(0);
+  const [imageFileUploadError, setImageFileUploadError] = useState(null);
+  const [uploadSuccess, setUploadSuccess] = useState(false); // New state for success alert
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setImageFile(file);
+      setImageFileUrl(URL.createObjectURL(file));
+    }
+  };
+
+  const uploadImage = useCallback(async () => {
+    if (!imageFile || !imageFile.name) {
+      setImageFileUploadError(null);
+
+      return;
+    }
+
+    setImageFileUploadError(null);
+    setUploadSuccess(false); // Reset success state before starting a new upload
+
+    const storage = getStorage(app);
+    const fileName = `${new Date().getTime()}_${imageFile.name}`;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, imageFile);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setImageFileUploadProgress(progress.toFixed(0));
+        console.log("Upload is " + progress + "% done");
+      },
+      (error) => {
+        setImageFileUploadError(
+          "Couldn't upload image (file must be less than 2MB)"
+        );
+        setImageFileUploadProgress(null);
+        setImageFile(null);
+        setImageFileUrl(null);
+        console.log(error);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setImageFile(downloadURL);
+          setUploadSuccess(true); // Set success state
+          setTimeout(() => {
+            setUploadSuccess(false); // Hide the success alert after 2 seconds
+          }, 2000);
+        });
+      }
+    );
+  }, [imageFile]);
+
+  useEffect(() => {
+    if (imageFile) {
+      uploadImage();
+    }
+  }, [imageFile, uploadImage]);
+
   return (
     <div className="max-w-lg mx-auto p-3 w-full">
       <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
       <form className="flex flex-col gap-4">
-        <div className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full ">
+        <input
+          type="file"
+          accept="image/*"
+          onChange={handleImageChange}
+          ref={filePickerRef}
+          hidden
+        />
+        <div
+          className="relative w-32 h-33 self-center cursor-pointer shadow-md overflow-hidden rounded-full"
+          onClick={() => {
+            filePickerRef.current.click();
+          }}
+        >
+          {imageFileUploadProgress && (
+            <CircularProgressbar
+              value={imageFileUploadProgress || 0}
+              text={`${imageFileUploadProgress}%`}
+              strokeWidth={5}
+              styles={{
+                root: {
+                  width: "100%",
+                  height: "100%",
+                  position: "absolute",
+                  top: 0,
+                  left: 0,
+                },
+                path: {
+                  stroke: `rgba(62,152,199, ${imageFileUploadProgress / 100})`,
+                },
+              }}
+            />
+          )}
           <img
-            src={currentUser.profilePicture}
+            src={imageFileUrl || currentUser.profilePicture}
             alt="user"
-            className="rounded-full w-full h-full object-cover border-8 border-[lightgray]"
+            className={`rounded-full w-full h-full object-cover ${
+              imageFileUploadProgress &&
+              imageFileUploadProgress < 100 &&
+              "opacity-60"
+            }`}
           />
         </div>
-        <TextInput type="text" id="username" placeholder="username" defaultValue={currentUser.username}/>
-        <TextInput type="email" id="email" placeholder="email" defaultValue={currentUser.email}/>
-        <TextInput type="password" id="password" placeholder="password" defaultValue="********"/>
-        <Button type="submit" gradientDuoTone='purpleToBlue' outline> Update </Button>
+        {imageFileUploadError && (
+          <Alert color="failure">{imageFileUploadError}</Alert>
+        )}
+        {uploadSuccess && (
+          <Alert color="success">Image successfully uploaded!</Alert>
+        )}
+
+        <TextInput
+          type="text"
+          id="username"
+          placeholder="username"
+          defaultValue={currentUser.username}
+        />
+        <TextInput
+          type="email"
+          id="email"
+          placeholder="email"
+          defaultValue={currentUser.email}
+        />
+        <TextInput
+          type="password"
+          id="password"
+          placeholder="password"
+          defaultValue="********"
+        />
+        <Button type="submit" gradientDuoTone="purpleToBlue" outline>
+          Update
+        </Button>
 
         <div className="text-red-500 flex justify-between mt-5 ">
-            <span className="cursor-pointer">Delete Account</span>
-            <span className="cursor-pointer">Sign Out</span>
-            
+          <span className="cursor-pointer">Delete Account</span>
+          <span className="cursor-pointer">Sign Out</span>
         </div>
       </form>
     </div>
