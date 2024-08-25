6bad245bee8c3e9ae10f2fe5329d466db44a6815
TalipOrdu <ordu.talip007@gmail.com>
2024-08-25 19:53:28 +0300

    create get posts api route

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -1,8 +1,10 @@
 import Post from "../models/post.model.js";
-import {errorHandler} from "../utils/error.js"
+import {
+    errorHandler
+} from "../utils/error.js"
 
 export const create = async (req, res, next) => {
-    
+
     if (!req.user.isAdmin) {
         return next(errorHandler(403, "You are not allow to create a post"));
     }
@@ -24,3 +26,43 @@ export const create = async (req, res, next) => {
         next(error);
     }
 }
+
+export const getposts = async (req, res, next) => {
+    try {
+        const startIndex = parseInt(req.query.startIndex) || 0;
+        const limit = parseInt(req.query.limit) || 9;
+        const sortDirection = req.query.order === 'asc' ? 1 : -1;
+        
+        const query = {};
+        
+        if (req.query.userId) query.userId = req.query.userId;
+        if (req.query.category) query.category = req.query.category;
+        if (req.query.slug) query.slug = req.query.slug;
+        if (req.query.postId) query._id = req.query.postId;
+        if (req.query.searchTerm) {
+            query.$or = [
+                { title: { $regex: req.query.searchTerm, $options: 'i' } },
+                { content: { $regex: req.query.searchTerm, $options: 'i' } },
+            ];
+        }
+
+        const posts = await Post.find(query)
+            .sort({ updatedAt: sortDirection })
+            .skip(startIndex)
+            .limit(limit);
+        
+        const totalPost = await Post.countDocuments();
+        const now = new Date();
+        const oneMonthAgo = new Date(now.getFullYear(), now.getMonth() - 1, now.getDate());
+        const lastMonthPosts = await Post.countDocuments({ createdAt: { $gte: oneMonthAgo } });
+
+        res.status(200).json({
+            posts,
+            totalPost,
+            lastMonthPosts,
+        });
+
+    } catch (error) {
+        next(error);
+    }
+};
