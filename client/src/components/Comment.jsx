3ef68d1b0054e29b7f7d9296a3868dad561414a2
TalipOrdu <ordu.talip007@gmail.com>
2024-09-05 16:55:34 +0300

    Edit comment function

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -1,11 +1,14 @@
 import { useEffect, useState } from "react";
 import moment from "moment";
-import PropTypes from 'prop-types';
-import {FaThumbsUp} from "react-icons/fa";
+import PropTypes from "prop-types";
+import { FaThumbsUp } from "react-icons/fa";
 import { useSelector } from "react-redux";
+import {Button, Textarea} from "flowbite-react";
 
-export default function Comment({ comment, onLike }) {
+export default function Comment({ comment, onLike, onEdit }) {
   const [user, setUser] = useState({});
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedContent, setEditingContent] = useState(comment.content);
   const { currentUser } = useSelector((state) => state.user);
 
   useEffect(() => {
@@ -22,6 +25,31 @@ export default function Comment({ comment, onLike }) {
     };
     getUser();
   }, [comment]);
+
+  const handleEdit = () => {
+    setIsEditing(true);
+    setEditingContent(comment.content);
+  };
+  const handleSave = async() =>{
+    try {
+      const res = await fetch(`/api/comment/editComment/${comment._id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ content: editedContent }),
+      });
+      if (res.ok) {
+        const updatedComment = await res.json();
+        setIsEditing(false);
+        // Update the comment content in the parent component
+        onEdit(updatedComment); // Pass the updated comment back to the parent
+      }
+    } catch (error) {
+      console.log(error);
+      
+    }
+  }
   return (
     <div className="flex p-4 border-b dark:border-gray-600 text-sm">
       <div className="flex-shrink-0 mr-3">
@@ -40,30 +68,64 @@ export default function Comment({ comment, onLike }) {
             {moment(comment.createdAt).fromNow()}
           </span>
         </div>
-        <p className="text-gray-400 pb-2">{comment.content}</p>
-        <div className="flex items-center pt-2 text-xs border-t dark:border-gray-700 max-w-fit gap-2">
-          <button type="button" onClick={()=>onLike(comment._id)} className={`text-gray-400 hover:text-blue-500 ${currentUser && 
-            comment.likes.includes(currentUser._id) && '!text-blue-500'
-          }`}>
-            <FaThumbsUp className="text-sm"/>
-          </button>
-          <p className="text-gray-500">
-            {comment.numberOfLikes>0 && comment.likes.length}
-          </p>
-        </div>
+        {isEditing ? (
+          <>
+          <Textarea
+            className="w-full p-2 text-gray-700 bg-gray-200 rounded-md resize-none focus:outline-none focus:bg-gray-100"
+            rows={3}
+            value={editedContent}
+            onChange={(e) => setEditingContent(e.target.value)}
+          />
+          <div className="flex justify-end gap-2 text-xs mt-2">
+            <Button type="button" size="sm" gradientDuoTone="purpleToBlue" onClick={handleSave}>Save</Button>
+            <Button type="button" size="sm" gradientDuoTone="purpleToBlue" outline onClick={()=> setIsEditing(false)}>Cancel</Button>
+          </div>
+          </>
+        ) : (
+          <>
+            <p className="text-gray-400 pb-2">{comment.content}</p>
+            <div className="flex items-center pt-2 text-xs border-t dark:border-gray-700 max-w-fit gap-2">
+              <button
+                type="button"
+                onClick={() => onLike(comment._id)}
+                className={`text-gray-400 hover:text-blue-500 ${
+                  currentUser &&
+                  comment.likes.includes(currentUser._id) &&
+                  "!text-blue-500"
+                }`}
+              >
+                <FaThumbsUp className="text-sm" />
+              </button>
+              <p className="text-gray-500">
+                {comment.numberOfLikes > 0 && comment.likes.length}
+              </p>
+              {currentUser &&
+                (currentUser._id === comment.userId || currentUser.isAdmin) && (
+                  <button
+                    type="button"
+                    className="text-gray-400 hover:text-blue-500"
+                    onClick={handleEdit}
+                  >
+                    Edit
+                  </button>
+                )}
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
 }
 
 Comment.propTypes = {
-    comment: PropTypes.shape({
-      userId: PropTypes.string.isRequired,
-      createdAt: PropTypes.string.isRequired,
-      content: PropTypes.string.isRequired,
-      _id: PropTypes.string.isRequired,
-      likes: PropTypes.arrayOf(PropTypes.string).isRequired,
-      numberOfLikes: PropTypes.number.isRequired,
-    }).isRequired,
-    onLike: PropTypes.func.isRequired,
+  comment: PropTypes.shape({
+    userId: PropTypes.string.isRequired,
+    createdAt: PropTypes.string.isRequired,
+    content: PropTypes.string.isRequired,
+    _id: PropTypes.string.isRequired,
+    likes: PropTypes.arrayOf(PropTypes.string).isRequired,
+    numberOfLikes: PropTypes.number.isRequired,
+  }).isRequired,
+  onLike: PropTypes.func.isRequired,
+  onEdit: PropTypes.func.isRequired,
 };
--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -90,7 +90,11 @@ export default function CommentSection({ postId }) {
       console.log(error);
     }
   };
-  
+  const handleEdit = (updatedComment) => {
+    setComments((comments) =>
+      comments.map((c) => (c._id === updatedComment._id ? updatedComment : c))
+    );
+  };
   return (
     <div className="max-w-2xl mx-auto w-full p-3">
       {currentUser ? (
@@ -153,7 +157,7 @@ export default function CommentSection({ postId }) {
             </div>
           </div>
           {comments.map((comment) => (
-            <Comment key={comment._id} comment={comment} onLike={handleLike} />
+            <Comment key={comment._id} comment={comment} onLike={handleLike} onEdit={handleEdit}/>
           ))}
         </>
       )}
